a7386e0f16fcbff06ddd7a8e4fc54e0293e4ef0e
Flavio Poletti <flavio@polettix.it>
2018-03-31 02:00:36 +0200

    Allow exporting functions in ::Util

--- lib/Memorator/Util.pm
+++ lib/Memorator/Util.pm
@@ -3,6 +3,9 @@ use strict;
 use warnings;
 { our $VERSION = '0.001'; }
 
+use Exporter qw< import >;
+our @EXPORT_OK = qw< local_name >;
+
 sub local_name {
    my ($name, $suffix) = @_;
    (my $retval = $name . '_' . $suffix) =~ s{\W}{_}gmxs;
